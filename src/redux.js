fdb548bc598de6ee2f62c8a29009924bd4a3f1ee
Ivan Gaenko <gaenko37@gmail.com>
2018-12-13 15:59:25 +0200

    init commit, add combineReducers

---
+++ src/redux.js
@@ -0,0 +1,114 @@
+import { createStore, combineReducers } from 'redux';
+
+const createId = () => Math.random();
+
+const books = (state = [], action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case 'ADD_BOOK':
+      return [
+        ...state,
+        {
+          id: createId(),
+          title: payload
+        }
+      ];
+    case 'UPDATE_BOOK':
+    return state.map((item) => {
+      if (item.id === payload.id) {
+        return {
+          ...item,
+          title: payload.newName,
+        }
+      }
+      return item;
+    });
+    case 'REMOVE_BOOK':
+    return state.filter(book => book.id !== payload);
+    default:
+    return state;
+  }
+}
+
+const readers = (state = [], action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case 'ADD_READER':
+    return [
+        ...state,
+        {
+        id: createId(),
+        title: payload
+        }
+      ]
+    case 'UPDATE_READER':
+    return state.map((item) => {
+      if (item.id === payload.id) {
+        return {
+          ...item,
+          title: payload.newName,
+        }
+      }
+      return item;
+    })
+    case 'REMOVE_READER':
+    return state.filter(reader => reader.id !== payload);
+    default:
+    return state;
+  }
+}
+
+const reducer = combineReducers({
+  books,
+  readers
+})
+
+const store = createStore(reducer);
+
+const addBook = (bookName) => ({
+  type: 'ADD_BOOK',
+  payload: bookName
+});
+
+const addReader = (bookName) => ({
+  type: 'ADD_READER',
+  payload: bookName
+});
+
+const removeBook = (bookName) => ({
+  type: 'REMOVE_BOOK',
+  payload: bookName
+});
+
+const removeReader = (bookName) => ({
+  type: 'REMOVE_READER',
+  payload: bookName
+});
+
+const updateBook = (id, newName) => ({
+  type: 'UPDATE_BOOK',
+  payload: { id, newName}
+});
+
+const updateReader = (id, newName) => ({
+  type: 'UPDATE_READER',
+  payload: { id, newName}
+});
+
+store.subscribe(() => {
+  console.log('From subscribe', store.getState());
+});
+
+store.dispatch(addBook('Book 1'));
+store.dispatch(addBook('Book 2'));
+store.dispatch(addBook('Book 3'));
+store.dispatch(addBook('Book 4'));
+store.dispatch(removeBook(store.getState().books[0].id));
+store.dispatch(updateBook(store.getState().books[0].id, 'Updated Book Title'));
+
+store.dispatch(addReader('Reader 1'));
+store.dispatch(addReader('Reader 2'));
+store.dispatch(addReader('Reader 3'));
+store.dispatch(addReader('Reader 4'));
+store.dispatch(removeReader(store.getState().readers[0].id));
+store.dispatch(updateReader(store.getState().readers[0].id, 'Updated Readers Title'));
